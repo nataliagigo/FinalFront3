d8003f3c8c27fc891ac2e7d3d43d1379637ba38f
Angélica Arias V <93158681+amava07@users.noreply.github.com>
2023-04-05 16:26:50 -0600

    img styles footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from '../Components/Styles/Footer.module.css'
 import { useEstadosGlobalesContext } from "./utils/DentistContext";
-import facebook from '../../public/images/ico-facebook.png'
 
 
 const Footer = () => {
@@ -9,6 +8,22 @@ const Footer = () => {
   return (
     <>
       <div className={styles.bar}>
+        <img
+          src='../images/ico-facebook.png'
+          alt='iconFacebook'
+          className={styles.socialneticon}
+        />
+        <img
+          src='../images/ico-instagram.png'
+          alt='iconInsta'
+          className={styles.socialneticon} />
+
+        <img
+          src='../images/ico-whatsapp.png'
+          alt='iconWhatsapp'
+          className={styles.socialneticon}
+        />
+
 
       </div>
 
@@ -16,13 +31,8 @@ const Footer = () => {
         <p className={styles.pFooter}>Powered by</p>
         <img src="../images/DH.png" alt="DH-logo" className={styles.img} />
       </footer >
-      <section>
-        <nav>
-          <a href=""></a>
-        </nav>
 
 
-      </section>
     </>
   );
 };
